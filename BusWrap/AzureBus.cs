8bfeb6c78d249b66a85ec2f7f3d87f5f9a4bfe8d
eas <esaksonov@alegeus.com>
2018-04-25 16:38:26 -0400

    initial commit

---
+++ BusWrap/AzureBus.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BusWrap
+{
+    /// <summary>
+    /// Summary description for AzureBus
+    /// </summary>
+    public class AzureBus : IBusWrap
+    {
+        public AzureBus()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+        public List<string> GetMessages()
+        {
+            List<string> messages = new List<string>();
+            messages.Add("AzureBus msg 1");
+            messages.Add("AzureBus msg 3");
+            messages.Add("AzureBus msg 2");
+            return messages;
+        }
+        public List<string> GetTopics()
+        {
+            List<string> topics = new List<string>();
+            topics.Add("AzureBus topic 3");
+            topics.Add("AzureBus topic 2");
+            topics.Add("AzureBus topic 1");
+            return topics;
+        }
+    }
+}
---
+++ BusWrap/KafkaBus.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusWrap
+{
+    /// <summary>
+    /// Summary description for KafkaBus
+    /// </summary>
+    public class KafkaBus : IBusWrap
+    {
+        public KafkaBus()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+        public List<string> GetMessages()
+        {
+            List<string> messages = new List<string>();
+            messages.Add("kafka msg 1");
+            messages.Add("kafka msg 3");
+            messages.Add("kafka msg 2");
+            return messages;
+        }
+        public List<string> GetTopics()
+        {
+            List<string> topics = new List<string>();
+            topics.Add("kafka topic 3");
+            topics.Add("kafka topic 2");
+            topics.Add("kafka topic 1");
+            return topics;
+        }
+    }
+}
